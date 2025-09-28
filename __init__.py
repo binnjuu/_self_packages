b7187c6e0abdc1bba6d97d0f985e537b0e13cf93
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-09-28 23:05:32 +0800

    新增 __init__.py, setup.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .chrome_driver import *
+from .kawaii_fox import *
+from .output import *
+from .send import *
+
+__all__ = [
+    "chrome_driver",
+    "kawaii_fox",
+    "output",
+    "send",
+]
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="_self_packages",
+    version="0.1.0",
+    description="自用套件包",
+    author="binnjuu",
+    author_email="binnjuu205@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
